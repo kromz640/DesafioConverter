b48c0fff2d8010affbe3802ad616e57e1c515149
Kronoz640 <rroquek@msn.com>
2024-04-24 20:05:42 -0600

    tercer commit

--- src/desafioconvertidor/alura/com/Monedas.java
+++ src/desafioconvertidor/alura/com/Monedas.java
@@ -1,6 +1,26 @@
 package desafioconvertidor.alura.com;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Map;
+
 public class Monedas
 {
 
+   // @serializedName("conversionrates");
+    @SerializedName("conversionrates")
+
+    private Map(String, Double) tasasCambio;
+
+    public Map<String, Double> getTasasCambio()
+    {
+
+        return tasasCambio;
+    }
+}
+
+Override
+public String toString()
+{
+    return "Tasas de cambio:" + tasasCambio;
 }
