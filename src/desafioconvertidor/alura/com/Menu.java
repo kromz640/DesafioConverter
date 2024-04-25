b0a85ab98f0a3f27ab4eb61528c35c1704724029
Kronoz640 <rroquek@msn.com>
2024-04-24 19:43:10 -0600

    primer commit

---
+++ src/desafioconvertidor/alura/com/Menu.java
@@ -0,0 +1,25 @@
+package desafioconvertidor.alura.com;
+
+
+
+public class Menu
+{
+    public void MOSTRAR_MENU()
+    {
+        System.out.println("""
+            CONVERSOR DE MONEDA UTILIZANDO API EXCHANGE RATES
+            
+            MENU
+            Bienvenido al conversor de moneda
+            1- Dólar a Peso Argentino
+            2- Peso Argentino a Dólar
+            3- Dólar a Real Brasileño
+            4- Real Brasileño a Dólar
+            5- Dólar a Peso Colombiano
+            6- Peso Colombiano a Dólar
+            7- Salir
+            
+            Elija una opción: """);
+    }
+
+}
---
+++ src/desafioconvertidor/alura/com/Principal.java
@@ -0,0 +1,19 @@
+package desafioconvertidor.alura.com;
+import java.awt.*;
+import java.util.Scanner;
+
+public class Principal
+{
+    public static void main(String[] args)
+    {
+    while(true)
+    {
+        byte bytOpcion;
+        Scanner sc = new Scanner(System.in);
+
+        Menu menu = new Menu();
+        menu.MostrarMenu();
+
+    }
+    }
+}
